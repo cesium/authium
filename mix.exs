1679b8beb2b3a0cd4f74a248a17b8492fc5d5d06
Rui Oliveira <70754369+ruioliveira02@users.noreply.github.com>
2022-10-15 15:15:03 +0100

    Add linter (#2)
    * Add linter
    
    * Fix lint

--- mix.exs
+++ mix.exs
@@ -48,7 +48,8 @@ defmodule Authium.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 
@@ -64,6 +65,7 @@ defmodule Authium.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      lint: ["credo --strict --all"],
       "assets.deploy": ["esbuild default --minify", "phx.digest"]
     ]
   end
